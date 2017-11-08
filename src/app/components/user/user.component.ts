d2002959cb16d6e2083ccb09fa6c7e72a3379105
Benjamin Champetier <bchampetier@amiltone.fr>
2017-11-08 12:08:58 +0100

    ajout interface Lorem

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,12 +7,45 @@ import { Component, OnInit } from '@angular/core';
   //encapsulation: ViewEncapsulation.None
 })
 export class UserComponent implements OnInit {
+  name: string;
+  age: number;
+  email: string;
+  address: Address;
+  lorem: Lorem;
 
-  name = 'Benjamin';
 
   constructor() { }
 
   ngOnInit() {
+    console.log('ngOnInit ran...');
+
+    let pasdidee = document.querySelector('.paragraph');
+    window.setInterval(function(){
+      pasdidee.classList.toggle('red');
+    }, 1000);
+
+
+    this.name = 'Benjamin';
+    this.age = 29;
+    this.address = {
+      street: '176 rue Fructidor',
+      city: 'Montpellier',
+      state: 'Languedoc'
+    };
+    this.lorem = {
+      questceque:`Qu'est-ce que le Lorem Ipsum?
+      Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.`
+    }
   }
 
+}
+
+interface Address {
+  street: string,
+  city: string,
+  state: string
+}
+
+interface Lorem {
+  questceque: string;
 }
